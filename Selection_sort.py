c84152697fc727174809cfee7961432d2dd7a548
osipyanarmen <osipyan87@yandex.ru>
2020-11-17 00:51:15 +0400

    Selection sort commit No2

--- Selection_sort.py
+++ Selection_sort.py
@@ -4,7 +4,8 @@ def sorted_list(lst):
         for j in range(i+1, len(lst)):
             if lst[j] < lst[low_value_index]:
                 low_value_index = j
-            lst[i], lst[low_value_index] = lst[low_value_index], lst[i]
+        lst[i], lst[low_value_index] = lst[low_value_index], lst[i]
+
 
 lst_input = []
 lst_len = int(input('Enter number of elements in list: '))
