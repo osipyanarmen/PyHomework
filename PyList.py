a8cb003c9559a2bfb90380c8ef44f98523c61ad5
osipyanarmen <osipyan87@yandex.ru>
2020-11-16 21:53:17 +0400

    first commit

---
+++ PyList.py
@@ -0,0 +1,17 @@
+lst1 = []
+lst2 = []
+lst_sum = 0
+elem_num = int(input('Enter the number of elements:  '))
+print('Enter list elements')
+for i in range(0, elem_num):
+    elem = int(input())
+    lst1.append(elem)
+    lst_sum = lst_sum + elem
+    if elem % 2 == 0:
+        lst2.append(elem)
+print('This is the full list')
+print(lst1, '\n')
+print('This is the enum numbers list')
+print(lst2, '\n')
+print('This is the sum of elements of main list')
+print(lst_sum, '\n')
