83d7b664f5188902405e00e271ae014a35c16ff9
osipyanarmen <osipyan87@yandex.ru>
2020-11-19 01:57:16 +0400

    18.11.2020 homework No3

--- work_with_string.py
+++ work_with_string.py
@@ -1,4 +1,4 @@
-# Count the symbols, upper case and lower case in inputed text
+# Count the symbols, count upper case and lower case in inputed text
 txt = input('Input text: ')
 txt_length = len(txt)
 upper_case = 0
@@ -13,10 +13,17 @@ print('Your text contains ' + str(upper_case) + ' upper case symbols')
 print('Your text contains ' + str(lower_case) + ' lower case symbols')
 
 # Reverse the inputed text and print
-
 print('The reverse of your text is: ')
 while txt_length != 0:
     print(txt[txt_length-1], end='')
     txt_length -= 1
+print('\n')
 
+# Print each word from text in new line
+print('Each word from text on new line:')
+for i in range(0, len(txt)):
+    if txt[i] != ' ':
+        print(txt[i], end='')
+    else:
+        print('')
 
