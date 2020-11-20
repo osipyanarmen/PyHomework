4e97f1bf1c664d90628351dc9f259d73254fd112
osipyanarmen <osipyan87@yandex.ru>
2020-11-20 13:10:36 +0400

    bubble sort commit No3

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -2,10 +2,11 @@ def bubble_sort(lst):
     swapped = True
     while swapped:
         swapped = False
-        for i in range (len(lst)-1):
-            if lst[i] > lst[i+1]:
-                lst[i], lst[i+1] = lst[i+1], lst[i]
+        for i in range(len(lst) - 1):
+            if lst[i] > lst[i + 1]:
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
                 swapped = True
+
 
 lst_input = []
 lst_len = int(input('Enter number of elements in list: '))
