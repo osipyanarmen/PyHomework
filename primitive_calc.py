19d4b31ee24240c16a45b14ae68a6500a55f70aa
osipyanarmen <osipyan87@yandex.ru>
2020-11-20 16:02:25 +0400

    primitive calc v. 2.0

--- primitive_calc.py
+++ primitive_calc.py
@@ -2,19 +2,32 @@
 
 def calculator(x: int, y: int, oper:str):
     if oper == '+':
-        print(x+y)
+        print(x+y, '\n')
     elif oper == '-':
-        print(x-y)
+        print(x-y, '\n')
     elif oper == '*':
-        print(x*y)
+        print(x*y, '\n')
     elif oper == '/':
-        print(x/y)
+        print(x/y, '\n')
     else:
         print('No such operand')
 
-
 a = int(input('X = '))
 b = int(input('Y = '))
-operand = input('Choose operand (+, -, /, *): ')
-print('X '+operand+' Y = ', end=' ')
-calculator(a, b, operand)
+while True:
+    print('Input the operator to make calculation,\n or input "N" for new values,\n or input "Q" to EXIT')
+
+    operand = input('Choose operand (+, -, /, *): ')
+    if operand in ['+', '-', '*']:
+        print('X ' + operand + ' Y = ', end=' ')
+        calculator(a, b, operand)
+    elif operand == '/' and b == 0:
+        print("You can't divide by 0, please input new value for Y")
+        b = int(input('Y = '))
+        print('X ' + operand + ' Y = ', end=' ')
+        calculator(a, b, operand)
+    elif operand.upper() == 'Q':
+        break
+    elif operand.upper() == 'N':
+        a = int(input('X = '))
+        b = int(input('Y = '))
